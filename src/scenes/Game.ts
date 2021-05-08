063fae05e550c22f4043bf5686f4d4d2eb50b3ef
Kostas Paganelis <paganelis@gmail.com>
2021-05-08 23:26:22 +0300

    Finished Character Logic with a StateMachine - Sidescrolling Platformer in Phaser 3.50 - Part 3

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,11 @@
 import Phaser from 'phaser'
+import PlayerController from './PlayerController';
 
 export default class Game extends Phaser.Scene {
 
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
     private penguin?: Phaser.Physics.Matter.Sprite;
-    private isTouchingGround: boolean = false;
+    private playerController?: PlayerController;
 
     constructor() {
         super('game');
@@ -21,7 +22,6 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
-        this.createPenguinAnimations();
         let map = this.make.tilemap({ key: 'tilemap' });
         let tileset = map.addTilesetImage('iceworld', 'tiles');
         let ground = map.createLayer('ground', tileset);
@@ -40,10 +40,9 @@ export default class Game extends Phaser.Scene {
                     this.penguin = this.matter.add.sprite(x + (width * 0.5), y, 'penguin')
                     .play('player-idle')
                     .setFixedRotation();
-                    
-                    this.penguin.setOnCollide((data: MatterJS.ICollisionPair) => {
-                        this.isTouchingGround = true;
-                    });
+
+                    this.playerController = new PlayerController(this.penguin, this.cursors);
+
                     this.cameras.main.startFollow(this.penguin);
                     break;
                 }
@@ -53,50 +52,13 @@ export default class Game extends Phaser.Scene {
         
     }
 
-    private createPenguinAnimations() {
-        this.anims.create({
-            key: 'player-idle',
-            frames: [{ 
-                key: 'penguin',
-                frame: 'penguin_walk01'
-            }]
-        })
-        this.anims.create({
-            key: 'player-walk',
-            frameRate: 10,
-            frames: this.anims.generateFrameNames('penguin', { 
-                start: 1, 
-                end: 4, 
-                prefix: 'penguin_walk0'
-            }),
-            repeat: -1
-        });
-    }
+    
 
-    update() {
-        if (!this.penguin) {
+    update(t: number, dt: number) {
+        if (!this.playerController) {
             return;
         }
 
-        let speed: number = 5;
-        if (this.cursors.left.isDown) {
-            this.penguin.flipX = true;
-            this.penguin.setVelocityX(-speed);
-            this.penguin.play('player-walk', true)
-        } else if (this.cursors.right.isDown) {
-            this.penguin.flipX = false;
-            this.penguin.setVelocityX(speed);
-            this.penguin.play('player-walk', true)
-        } else {
-            this.penguin.setVelocityX(0);
-            this.penguin.play('player-idle', true)
-        }
-
-        let spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
-        if (spaceJustPressed && this.isTouchingGround) {
-            this.penguin.setVelocityY(-12);
-            this.isTouchingGround = false;
-        }
-        
+        this.playerController.update(dt);
     }
 }
---
+++ src/scenes/PlayerController.ts
@@ -0,0 +1,128 @@
+import Phaser, { Physics } from 'phaser'
+import StateMachine from '../statemachine/StateMachine';
+
+type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+type Sprite = Phaser.Physics.Matter.Sprite;
+
+export default class PlayerController {
+    private sprite: Sprite;
+    private cursors: CursorKeys;
+    private stateMacine: StateMachine
+
+    constructor(sprite: Sprite, cursors: CursorKeys) {
+        this.sprite = sprite;
+        this.cursors = cursors;
+
+        this.createAnimations();
+        this.stateMacine = new StateMachine(this, 'player');
+
+        this.stateMacine
+        .addState('idle', {
+            onEnter: this.idleOnEnter,
+            onUpdate: this.idleOnUpdate
+        })
+        .addState('walk', {
+            onEnter: this.walkOnEnter,
+            onUpdate: this.walkOnUpdate,
+        })
+        .addState('jump', {
+            onEnter: this.jumpOnEnter,
+            onUpdate: this.jumpOnUpdate,
+        })
+        .setState('idle');
+        
+        this.sprite.setOnCollide((data: MatterJS.ICollisionPair) => {
+            if (this.stateMacine.isCurrentState('jump') {
+                this.stateMacine.setState('idle');
+            }
+        });
+        
+    }
+
+    update(dt: number) {
+        this.stateMacine.update(dt);
+    }
+
+    private idleOnEnter() {
+        this.sprite.play('player-idle');
+    }
+
+    private idleOnUpdate() {
+        if (this.cursors.left.isDown || this.cursors.right.isDown) {
+            this.stateMacine.setState('walk');
+        }
+        this.jumpOnSpacePressed();
+    }
+
+    private walkOnEnter() {
+        this.sprite.play('player-walk');
+    }
+
+    private walkOnUpdate() {
+        this.walkOnArrowsKeysPressed();
+        
+        if (!this.cursors.left.isDown && !this.cursors.right.isDown) {
+            this.sprite.setVelocityX(0);
+            this.stateMacine.setState('idle');
+        }
+
+        this.jumpOnSpacePressed();
+    }
+
+    private jumpOnEnter() {
+        this.sprite.play('player-jump');
+        this.sprite.setVelocityY(-12);
+    }
+
+    private jumpOnUpdate() {
+        this.walkOnArrowsKeysPressed()
+    }
+    
+    private walkOnArrowsKeysPressed() {
+        let speed: number = 5;
+        if (this.cursors.left.isDown) {
+            this.sprite.flipX = true;
+            this.sprite.setVelocityX(-speed);
+        } else if (this.cursors.right.isDown) {
+            this.sprite.flipX = false;
+            this.sprite.setVelocityX(speed);
+        }
+    }
+
+    private jumpOnSpacePressed() {
+        let spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+        if (spaceJustPressed) {
+            this.stateMacine.setState('jump');
+        }
+    }
+
+    private createAnimations() {
+        this.sprite.anims.create({
+            key: 'player-idle',
+            frames: [{
+                key: 'penguin',
+                frame: 'penguin_walk01'
+            }]
+        })
+        this.sprite.anims.create({
+            key: 'player-walk',
+            frameRate: 10,
+            frames: this.sprite.anims.generateFrameNames('penguin', {
+                start: 1,
+                end: 4,
+                prefix: 'penguin_walk0'
+            }),
+            repeat: -1
+        });
+        this.sprite.anims.create({
+            key: 'player-jump',
+            frameRate: 10,
+            frames: this.sprite.anims.generateFrameNames('penguin', {
+                start: 1,
+                end: 3,
+                prefix: 'penguin_jump0'
+            }),
+            repeat: -1
+        });
+    }
+}
